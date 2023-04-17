c6f95547e0dccd46e5523af99095e8ac7d5caffe
andreiadourado <106832026+andreiadourado@users.noreply.github.com>
2023-04-17 13:04:37 -0300

    Seção 07

--- JavaScript/js/remove-paciente.js
+++ JavaScript/js/remove-paciente.js
@@ -1,1 +1,9 @@
-
+var tabela = document.querySelector('#tabela-pacientes');
+tabela.addEventListener('dblclick', function(event)){
+    event.target.parentNode.classList.add('fadeOut'); //adicionando a classe
+      
+    setTimeout(function(){ // tempo de espera da remoção
+        event.target.parentNode.remove();//removendo a linha clicada - target: identifica a td clicada - parentNode: identifica a tr da td para apagar
+    }, 500);
+                       
+});               
