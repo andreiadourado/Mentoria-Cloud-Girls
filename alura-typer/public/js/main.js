6ad11004373c58beede4bae45d3ba015bcf28831
andreiadourado <andreia15@ov.ufrj.br>
2023-04-27 11:06:45 -0300

    Eventos

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -3,3 +3,18 @@ var numPalavras = frase.split(" ").length; //contando as palavras
 
 var tamanhoFrase = $("#tamanho-frase");
 tamanhoFrase.text(numPalavras);
+
+/*escutando o evento click na caixa de digitação*/
+var campo = $(".campo-digitacao");/*selecionando o campo*/ 
+campo.on("input", function() { /*quando digitar, fazer algo*/
+    console.log(campo.val()); /*value/conteúdo*/
+
+    /*contagem*/
+    var qtdPalavras = conteudo.split(/\S+/).length - 1; /*/\S+/: qualquer caractere, exceto espaço */
+    $("#contador-palavras").text(qtdPalavras);
+
+    var qtdCaracteres = conteudo.length;
+    $("#contador-caracteres").text(qtdCaracteres);
+
+});
+
