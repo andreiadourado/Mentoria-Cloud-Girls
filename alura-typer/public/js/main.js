48477cb1c44f35032e43d3462087a22cb40c5400
andreiadourado <andreia15@ov.ufrj.br>
2023-04-27 11:54:31 -0300

    Game Over com eventos - alteração

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -7,7 +7,7 @@ tamanhoFrase.text(numPalavras);
 /*escutando o evento click na caixa de digitação*/
 var campo = $(".campo-digitacao");/*selecionando o campo*/ 
 campo.on("input", function() { /*quando digitar, fazer algo*/
-    console.log(campo.val()); /*value/conteúdo*/
+    var conteudo = campo.val(); /*value/conteúdo*/
 
     /*contagem*/
     var qtdPalavras = conteudo.split(/\S+/).length - 1; /*/\S+/: qualquer caractere, exceto espaço */
