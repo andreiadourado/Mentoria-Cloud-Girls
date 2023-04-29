13236348c91de1a69f1699a388c905e6ec7c25da
andreiadourado <andreia15@ov.ufrj.br>
2023-04-29 09:40:21 -0300

    Enviar dados com AJAX

--- alura-typer/public/js/frases.js
+++ alura-typer/public/js/frases.js
@@ -1,4 +1,5 @@
 $("#botao-frase").click(fraseAleatoria);
+$("#botao-frase-id").click(buscaFrase);
 
 function fraseAleatoria() {
     $("#spinner").toggle(); 
@@ -22,4 +23,35 @@ function trocaFraseAleatoria(data) {
     frase.text(data[numeroAleatorio].texto);
     atualizaTamanhoFrase();
     atualizaTempoInicial(data[numeroAleatorio].tempo);
+}
+
+//arquivo frase.js
+function buscaFrase() {
+
+    $("#spinner").toggle();
+    var fraseId = $("#frase-id").val();
+
+    var dados = {id : fraseId}; //criando o objeto que guarda a id
+
+    //passando objeto como parâmetro
+    $.get("http://localhost:3000/frases", dados, trocaFrase)
+    .fail(function(){
+        $("#erro").toggle();
+        setTimeout(function(){
+            $("#erro").toggle();
+        },2000);
+    })
+    .always(function(){
+        $("#spinner").toggle();
+    });
+}
+
+function trocaFrase(data) {
+
+    console.log(data);
+
+    var frase = $(".frase");
+    frase.text(data.texto); 
+    atualizaTamanhoFrase();
+    atualizaTempoInicial(data.tempo);
 }
